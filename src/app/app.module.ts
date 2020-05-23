1c971688072b4c6cdf2b75079ad288c59af03169
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-05-23 17:08:08 +0200

    Typo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MovieCoverComponent } from './shared/movie-cover/movie-cover.component'
 import { MovieDetailsComponent } from './pages/movies/movie-details/movie-details.component';
 import { MoviesComponent } from './pages/movies/movies.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
-import { MovesInCategoryComponent } from './pages/categories/movies-in-category/moves-in-category.component';
+import { MoviesInCategoryComponent } from './pages/categories/movies-in-category/movies-in-category.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -19,7 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     MoviesComponent,
     CategoriesComponent,
     PageNotFoundComponent,
-    MovesInCategoryComponent,
+    MoviesInCategoryComponent,
     MovieCoverComponent,
   ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
