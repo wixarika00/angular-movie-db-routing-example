53f7886eb07cd7a3e4d5ba588a9f130302bd62ba
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-05-23 11:59:39 +0200

    Added router param

--- src/app/pages/movies/movie-details/movie-details.component.ts
+++ src/app/pages/movies/movie-details/movie-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpService } from '../../../services/http.service';
 import { Movie } from '../../../models/movie';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie-details',
@@ -11,12 +13,13 @@ import { Movie } from '../../../models/movie';
 export class MovieDetailsComponent implements OnInit {
   movieDetails: Observable<Movie>;
 
-  constructor(private http: HttpService) {
-  }
+  constructor(private http: HttpService, private route: ActivatedRoute) {}
 
   ngOnInit() {
+   this.movieDetails = this.route.paramMap.pipe(
+     switchMap((params: ParamMap) => this.http.getMovie(params.get('id')))
+   )
   }
 
-  goToMovies() {
-  }
+  goToMovies() {}
 }
