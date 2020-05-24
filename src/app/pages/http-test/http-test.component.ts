2a8ec51c210a7b3d7124822b18a39b0109a7efdd
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-05-24 16:59:30 +0200

    Http Starter

---
+++ src/app/pages/http-test/http-test.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-http-test',
+  templateUrl: './http-test.component.html',
+  styleUrls: ['./http-test.component.css'],
+})
+export class HttpTestComponent {
+  constructor() {}
+
+  get() {}
+
+  post() {}
+
+  put() {}
+
+  patch() {}
+
+  delete() {}
+}
